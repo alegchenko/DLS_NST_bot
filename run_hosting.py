074c7e9a09812cba8825206f33e0c90f8a7f9fc0
alegchenko <68243701+alegchenko@users.noreply.github.com>
2022-02-14 03:04:52 +0700

    Add files via upload

--- run_hosting.py
+++ run_hosting.py
@@ -1,7 +1,7 @@
 
-from utils.RES_NST import  Res_NST
-from utils.VGG_NST import VGG_NST
-from utils.Full_bot import Full_bot
+from utils.RES_NST import  Res_NST #класс умеющий делать NST с помошью resnet 50/101/152
+from utils.VGG_NST import VGG_NST #класс умеющий делать NST с помошью vgg 16/19
+from utils.Full_bot import Full_bot #класс содержаший функционал прошлых моделей и + super resolution
 import torch
 
 #Назначим вычисления на cpu
@@ -11,7 +11,7 @@ device = "cpu"
 vgg = torch.load("weights/vgg")
 res = torch.load("weights/ResNet")
 bot = Full_bot(vgg,res)
-bot.start_bot("5110316882:AAEXjriZPBJBP5xUqOYWoAm3XhU3H08ceYk")
+
 while True:
 	try:
 		bot.start_bot("5110316882:AAEXjriZPBJBP5xUqOYWoAm3XhU3H08ceYk")
