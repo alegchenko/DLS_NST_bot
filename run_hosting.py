40da26e24367013df3d31b13941e0a9c2f56e9b1
alegchenko <68243701+alegchenko@users.noreply.github.com>
2022-02-14 05:14:23 +0700

    Add files via upload

--- run_hosting.py
+++ run_hosting.py
@@ -4,14 +4,19 @@ from utils.VGG_NST import VGG_NST #класс умеющий делать NST с
 from utils.Full_bot import Full_bot #класс содержаший функционал прошлых моделей и + super resolution
 import torch
 
-#Назначим вычисления на cpu
+#Основная моя работа импортированные выше классы, большое кол-во других файлов относиться к готовой реализации SRGAN
+
+#Назначим вычисления на gpu/cpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #Загрузим веса моделей
 vgg = torch.load("weights/vgg").to(device).eval()
 res = torch.load("weights/ResNet").to(device).eval()
 bot = Full_bot(vgg,res)
-bot.start_bot("5110316882:AAEXjriZPBJBP5xUqOYWoAm3XhU3H08ceYk")
+
+#В моей реализации хостинг организованн ввиде метода для класса NST, но при этом классическое использование класса также возможно
+
+#Цикл используется чтобы мог перезапуститься в случаи сбоев
 while True:
 	try:
 		bot.start_bot("5110316882:AAEXjriZPBJBP5xUqOYWoAm3XhU3H08ceYk")
